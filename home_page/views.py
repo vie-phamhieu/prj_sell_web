d4eb4174174a50f475f0aa360a7bce5d0a88d7e3
vie.kougen <phamhieu.itj@gmail.com>
2024-04-03 01:19:49 +0700

    update 3/4

--- home_page/views.py
+++ home_page/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from . import views
 
 # Create your views here.
-def index(request):
-    return render(request, 'home_page/index.html')
+def homepage(request):
+    return render(request, 'home_page/home.html')
